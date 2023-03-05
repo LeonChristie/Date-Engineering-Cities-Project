bcfbd9cbdacd4306d1aa7cc1ff873ce404949f53
LeonChristie <110294002+LeonChristie@users.noreply.github.com>
2023-03-05 14:51:01 +0000

    caching feature built

--- main.py
+++ main.py
@@ -6,28 +6,53 @@ from google.cloud import bigquery
 from google.cloud import storage
 from google.cloud.bigquery import SchemaField
 from pprint import pprint
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Optional
 import time
 
-
+# Flights Lab API 
 API_KEY = os.environ.get("FLIGHTS_API_KEY")
 URL = f"https://app.goflightlabs.com/historical/2023-02-01?access_key={API_KEY}&code=LHW&type=arrival&date_to=2023-02-25&codeshared=null"
+
+
+# BigQuery
 CREDENTIALS_PATH = "/Users/leonchristie/Documents/GitHub/Date-Engineering-Cities-Project/pythonflightsbq.privatekey.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH
 BQ_CLIENT = bigquery.Client()
 TABLE_ID = "flights-project-379412.heathrow.heathrow-flights"
 
 
-# Fetch flight data and save to json file
-response = requests.get(URL)
-data = response.json()
-flight_data: List[Dict[str, Any]] = data["data"]
 
-# Dump to NDJSON
-with open("flights_data_nld.ndjson", "w") as f:
-    ndjson.dump(flight_data, f)
+# API call and caching
+def fetch_data(*, update: bool = False, json_cache: str, URL: str):
+    if update:
+        flight_data = None
+    else:
+        try:
+            with open(json_cache, 'r') as file:
+                flight_data = json.load(file)
+                print('Data fetched from local cache.')
+        except(FileNotFoundError, json.JSONDecodeError) as e:
+                print(f'no local cache found... ({e})')
+                flight_data = None
+
+    if not flight_data:
+        print('Fetching new json data... (Creating Local Cache)')
+        json_data = requests.get(URL).json()
+        flight_data: Optional[List[Dict[str, Any]]] = json_data["data"]
+        with open (json_cache, 'w') as file:
+            ndjson.dump(flight_data, file)
+
+    return flight_data
+
+if __name__ == '__main__':
+    json_cache = "Cached Data/json_cache.ndjson"
+  
+    data = fetch_data(update=False, json_cache=json_cache, URL=URL)
 
 
+
+
+# Schema
 custom_schema = [
     bigquery.SchemaField("type", "string", mode="NULLABLE"),
     bigquery.SchemaField("status", "string", mode="NULLABLE"),
@@ -82,13 +107,14 @@ custom_schema = [
     ),
 ]
 
+# Config
 job_config = bigquery.LoadJobConfig(
     schema=custom_schema,
     source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
     write_disposition="WRITE_APPEND",
 )
 
-
+# Send data to BigQeury
 with open("flights_data_nld.json", "rb") as source_file:
     job = BQ_CLIENT.load_table_from_file(source_file, TABLE_ID, job_config=job_config)
 
