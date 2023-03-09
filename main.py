acb981d192b3e4be6c765133fce4d072f41f2e50
LeonChristie <110294002+LeonChristie@users.noreply.github.com>
2023-03-09 18:25:24 +0000

    pre-commits test

--- main.py
+++ main.py
@@ -1,123 +1,148 @@
-import requests
-import json
-import ndjson
 import os
-from google.cloud import bigquery
-from google.cloud import storage
-from google.cloud.bigquery import SchemaField
-from pprint import pprint
-from typing import Dict, List, Any, Optional
-import time
+from typing import Any, Dict, List, Optional
 
-# Flights Lab API 
+import ndjson
+import requests
+from google.cloud import bigquery
+from google.cloud.bigquery import SchemaField
+
+# Flights Lab API
 API_KEY = os.environ.get("FLIGHTS_API_KEY")
-URL = f"https://app.goflightlabs.com/historical/2023-02-01?access_key={API_KEY}&code=LHW&type=arrival&date_to=2023-02-25&codeshared=null"
+URL = f"https://app.goflightlabs.com/historical/2023-02-15?access\
+    _key={API_KEY}&code=LHW&type=arrival&date_to=2023-02-16&codeshared=null"
 
 
 # BigQuery
-CREDENTIALS_PATH = "/Users/leonchristie/Documents/GitHub/Date-Engineering-Cities-Project/pythonflightsbq.privatekey.json"
+CREDENTIALS_PATH = "pythonflightsbq.privatekey.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH
 BQ_CLIENT = bigquery.Client()
 TABLE_ID = "flights-project-379412.heathrow.heathrow-flights"
 
 
-
-# API call and caching
-def fetch_data(*, update: bool = False, json_cache: str, URL: str):
-    if update:
-        flight_data = None
-    else:
-        try:
-            with open(json_cache, 'r') as file:
-                flight_data = json.load(file)
-                print('Data fetched from local cache.')
-        except(FileNotFoundError, json.JSONDecodeError) as e:
-                print(f'no local cache found... ({e})')
-                flight_data = None
-
-    if not flight_data:
-        print('Fetching new json data... (Creating Local Cache)')
+# Data caching
+def data_cacher(use_cache: bool, json_cache: str, URL: str) -> bool:
+    if use_cache:
+        # Check if cached data exists
+        _cache_exists: bool = os.path.exists(json_cache)
+        if _cache_exists:
+            return True
+        else:
+            json_data = requests.get(URL).json()
+            flight_data: Optional[List[Dict[str, Any]]] = json_data["data"]
+            with open(json_cache, "w") as file:
+                ndjson.dump(flight_data, file)
+            return True
+    elif not use_cache:
         json_data = requests.get(URL).json()
-        flight_data: Optional[List[Dict[str, Any]]] = json_data["data"]
-        with open (json_cache, 'w') as file:
+        flight_data = json_data["data"]
+        with open(json_cache, "w") as file:
             ndjson.dump(flight_data, file)
-
-    return flight_data
-
-if __name__ == '__main__':
-    json_cache = "Cached Data/json_cache.ndjson"
-  
-    data = fetch_data(update=False, json_cache=json_cache, URL=URL)
-
-
+        print("There was no data in the cache, force fetching data...")
+        print("Data cached for optional use later")
+        return True
+    return False
 
 
 # Schema
 custom_schema = [
-    bigquery.SchemaField("type", "string", mode="NULLABLE"),
-    bigquery.SchemaField("status", "string", mode="NULLABLE"),
-    bigquery.SchemaField(
+    SchemaField("type", "STRING", mode="NULLABLE"),
+    SchemaField("status", "STRING", mode="NULLABLE"),
+    SchemaField(
         "departure",
-        "string",
-        mode="NULLABLE",
+        "RECORD",
         fields=[
-            bigquery.SchemaField("iataCode", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iacoCode", "string", mode="NULLABLE"),
-            bigquery.SchemaField("terminal", "string", mode="NULLABLE"),
-            bigquery.SchemaField("gate", "string", mode="NULLABLE"),
-            bigquery.SchemaField("scheduledTime", "string", mode="NULLABLE"),
-            bigquery.SchemaField("estimatedTime", "string", mode="NULLABLE"),
-            bigquery.SchemaField("actualTime", "string", mode="NULLABLE"),
-            bigquery.SchemaField("estimatedRunway", "string", mode="NULLABLE"),
-            bigquery.SchemaField("actualRunway", "string", mode="NULLABLE"),
+            SchemaField("iataCode", "STRING", mode="NULLABLE"),
+            SchemaField("iacoCode", "STRING", mode="NULLABLE"),
+            SchemaField("terminal", "STRING", mode="NULLABLE"),
+            SchemaField("gate", "STRING", mode="NULLABLE"),
+            SchemaField("delay", "INT64", mode="NULLABLE"),
+            SchemaField("scheduledTime", "STRING", mode="NULLABLE"),
+            SchemaField("estimatedTime", "STRING", mode="NULLABLE"),
+            SchemaField("actualTime", "STRING", mode="NULLABLE"),
+            SchemaField("estimatedRunway", "STRING", mode="NULLABLE"),
+            SchemaField("actualRunway", "STRING", mode="NULLABLE"),
         ],
     ),
-    bigquery.SchemaField(
+    SchemaField(
         "arrival",
-        "string",
-        mode="NULLABLE",
+        "RECORD",
         fields=[
-            bigquery.SchemaField("iataCode", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iacoCode", "string", mode="NULLABLE"),
-            bigquery.SchemaField("terminal", "string", mode="NULLABLE"),
-            bigquery.SchemaField("baggage", "string", mode="NULLABLE"),
-            bigquery.SchemaField("scheduledTime", "string", mode="NULLABLE"),
-            bigquery.SchemaField("estimatedTime", "string", mode="NULLABLE"),
+            SchemaField("iataCode", "STRING", mode="NULLABLE"),
+            SchemaField("iacoCode", "STRING", mode="NULLABLE"),
+            SchemaField("terminal", "STRING", mode="NULLABLE"),
+            SchemaField("baggage", "STRING", mode="NULLABLE"),
+            SchemaField("delay", "INT64", mode="NULLABLE"),
+            SchemaField("scheduledTime", "STRING", mode="NULLABLE"),
+            SchemaField("estimatedTime", "STRING", mode="NULLABLE"),
         ],
     ),
-    bigquery.SchemaField(
+    SchemaField(
         "airline",
-        "string",
-        mode="NULLABLE",
+        "RECORD",
         fields=[
-            bigquery.SchemaField("name", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iatoCode", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iacoCode", "string", mode="NULLABLE"),
+            SchemaField("name", "STRING", mode="NULLABLE"),
+            SchemaField("iataCode", "STRING", mode="NULLABLE"),
+            SchemaField("iacoCode", "STRING", mode="NULLABLE"),
         ],
     ),
-    bigquery.SchemaField(
+    SchemaField(
         "flight",
-        "string",
-        mode="NULLABLE",
+        "RECORD",
         fields=[
-            bigquery.SchemaField("number", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iatoNaumber", "string", mode="NULLABLE"),
-            bigquery.SchemaField("iacoNumber", "string", mode="NULLABLE"),
+            SchemaField("number", "STRING", mode="NULLABLE"),
+            SchemaField("iataNaumber", "STRING", mode="NULLABLE"),
+            SchemaField("iacoNumber", "STRING", mode="NULLABLE"),
+        ],
+    ),
+    SchemaField(
+        "codeshared",
+        "RECORD",
+        fields=[
+            SchemaField(
+                "airline",
+                "RECORD",
+                fields=[
+                    SchemaField("name", "STRING", mode="NULLABLE"),
+                    SchemaField("iataCode", "STRING", mode="NULLABLE"),
+                    SchemaField("iacoCode", "STRING", mode="NULLABLE"),
+                ],
+            ),
+            SchemaField(
+                "flight",
+                "RECORD",
+                fields=[
+                    SchemaField("number", "STRING", mode="NULLABLE"),
+                    SchemaField("iataNumber", "STRING", mode="NULLABLE"),
+                    SchemaField("iacoNumber", "STRING", mode="NULLABLE"),
+                ],
+            ),
         ],
     ),
 ]
 
-# Config
-job_config = bigquery.LoadJobConfig(
-    schema=custom_schema,
-    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
-    write_disposition="WRITE_APPEND",
-)
 
-# Send data to BigQeury
-with open("flights_data_nld.json", "rb") as source_file:
-    job = BQ_CLIENT.load_table_from_file(source_file, TABLE_ID, job_config=job_config)
+def main(URL: str, use_cache: bool, json_cache: str):
 
-response = job.result()
+    if data_cacher(use_cache, json_cache, URL):
+        # # Config
+        job_config = bigquery.LoadJobConfig(
+            schema=custom_schema,
+            source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+            write_disposition="WRITE_APPEND",
+            ignore_unknown_values=True,
+        )
+        # # Send data to BigQeury
+        with open("cached_data/json_cache.ndjson", "rb") as source_file:
+            job = BQ_CLIENT.load_table_from_file(
+                source_file, TABLE_ID, job_config=job_config
+            )
+            print(job.state)
+            job.result()
+            print(job.state)
 
 
+if __name__ == "__main__":
+    use_cache = True
+    json_cache = "cached_data/json_cache.ndjson"
+    URL = URL
+    main(URL, use_cache, json_cache)
